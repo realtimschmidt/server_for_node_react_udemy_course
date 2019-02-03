57ca24396c4424ae6e30f0ec3ee4daeef412cbaa
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-03 16:19:15 -0500

    use the login button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return (
+          <li><a href="/auth/google">Login with Google</a></li>
+        )
+      default:
+        return <li><a>Logout</a></li>
+    }
+  }
+  
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
-            Super Cool Surveys
-          </a>
+          <a className="left brand-logo">Super Cool Surveys</a>
           <ul className="right">
-            <li>Login With Google</li>
+            <li>
+              {this.renderContent()}
+            </li>
           </ul>
         </div>
       </nav>
@@ -17,4 +31,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
