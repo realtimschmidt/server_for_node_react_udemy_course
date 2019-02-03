4a1c8c9efabdb1c21edc44c03b72dae4cdb0784e
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-03 14:42:10 -0500

    dummy header and component setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,9 +6,19 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-
 const App = () => {
-  return <div>Hi Meg!</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
