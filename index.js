58628ca66f898c3e1cec9e6f46ed248dfa977a06
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-02 22:31:46 -0500

    find user if it already exists in DB

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
 const authRoutes = require('./routes/authRoutes')
-require('./services/passport')
 require('./models/user')
+require('./services/passport')
 
 mongoose.connect(keys.mongoURI)
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
+
+const User = mongoose.model('users')
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,12 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log({ accessToken })
-      console.log({ refreshToken })
-      console.log({ profile })
+      User.findOne({ googleId: profile.id }).then(foundUser => {
+        if (foundUser) {
+        } else {
+          new User({ googleId: profile.id }).save()
+        }
+      })
     }
   )
 )
