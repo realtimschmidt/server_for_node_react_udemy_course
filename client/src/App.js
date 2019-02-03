7edd850709cb31b20df4460963ecb0801c1d49d5
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-03 13:32:47 -0500

    refactor passport.js to use async await

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi Meg!
+          </p>
+          <p>
+          👋
+          </p>
+          <a href="/auth/google">Sign In With Google</a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
--- services/passport.js
+++ services/passport.js
@@ -23,16 +23,13 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(foundUser => {
-        if (foundUser) {
-          done(null, foundUser)
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user))
-        }
-      })
+    async (accessToken, refreshToken, profile, done) => {
+      const foundUser = await User.findOne({ googleId: profile.id })
+      if (foundUser) {
+        return done(null, foundUser)
+      }
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user)
     }
   )
 )
