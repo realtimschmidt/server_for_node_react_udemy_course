c05822595e0cb8cb0de43085545568898c46e6b5
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-02 23:00:18 -0500

    add cookie key

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
 const authRoutes = require('./routes/authRoutes')
 require('./models/user')
@@ -8,6 +10,14 @@ require('./services/passport')
 mongoose.connect(keys.mongoURI)
 
 const app = express()
+
+const days30 = 30 * 24 * 60 * 60 * 1000
+app.use(
+  cookieSession({
+    maxAge: days30,
+    keys: [keys.cookieKey]
+  })
+)
 
 authRoutes(app)
 
