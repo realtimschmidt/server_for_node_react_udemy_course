a360c66d5ef023e5c854a00f297c9cc183d90a2a
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-05 23:48:12 -0500

    create charge object

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,7 +4,7 @@ const stripe = require('stripe')(keys.stripeSecretKey)
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
     if (!req.user) {
-      return res.status(401)
+      return res.status(401).send({ error: 'You must log in!' })
     }
     const charge = await stripe.charges.create({
       amount: 500,
