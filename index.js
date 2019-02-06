00bc8e767327012856d2938a4be919e636be467f
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-05 23:47:33 -0500

    wip add stripe payments to backend

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cookieSession = require('cookie-session')
 const passport = require('passport')
 const keys = require('./config/keys')
 const authRoutes = require('./routes/authRoutes')
+const billingRoutes = require('./routes/billingRoutes')
+const bodyParser = require('body-parser')
 require('./models/user')
 require('./services/passport')
 
@@ -11,6 +13,7 @@ mongoose.connect(keys.mongoURI)
 
 const app = express()
 
+app.use(bodyParser.json())
 const days30 = 30 * 24 * 60 * 60 * 1000
 app.use(
   cookieSession({
@@ -22,6 +25,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 authRoutes(app)
+billingRoutes(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
