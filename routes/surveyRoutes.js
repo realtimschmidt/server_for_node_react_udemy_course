563421f11221af1494b0f06c1e02bdff136067c4
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-06 23:02:33 -0500

    mailer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,27 @@
+const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
+const Mailer = require('../services/Mailer')
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
+
+const Survey = mongoose.model('surveys')
 
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body
+
+    const recipientEmails = recipients.split(',').map(email => ({ email: email.trim() }))
     
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipientEmails,
+      _user: req.user.id,
+      dateSent: Date.now()
+    })
+
+    // great place to send an email
+    const mailer = new Mailer(survey, surveyTemplate(survey))
   })
 }
