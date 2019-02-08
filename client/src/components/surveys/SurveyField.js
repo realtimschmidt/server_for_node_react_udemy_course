68d23c24d6a0361bf712be0f31a8c669326f6f42
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-07 20:16:10 -0500

    wip SurveyField wired to SurveyForm

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,12 @@
+// SurveyField contains logic to render a single
+// lable and text input
+import React from 'react'
+
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  )
+}
