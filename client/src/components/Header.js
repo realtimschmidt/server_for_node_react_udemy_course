21996815f33671fb05296fffd0196de8a348fc23
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-07 20:36:45 -0500

    form butons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,11 +19,11 @@ class Header extends Component {
           <li key="1">
             <Payments />
           </li>,
-          <li key="3" style={{ margin: '0 10px' }}>
-            Credits: {this.props.auth.credits}
-          </li>,
           <li key="2">
             <a href="/api/logout">Logout</a>
+          </li>,
+          <li key="3" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
           </li>
         ]
     }
