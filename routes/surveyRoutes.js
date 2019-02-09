f639acc3cd802b33fa3c36bf0be4c678b040aad3
realtimschmidt <schmidt.tim.j@gmail.com>
2019-02-09 15:50:23 -0500

    filter out duplicate responses

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,5 @@
 const _ = require('lodash')
-const Path = require('path-parser')
+const { Path } = require('path-parser')
 const { URL } = require('url')
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
@@ -15,11 +15,23 @@ module.exports = app => {
   })
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, (event) => {
-      const pathname = new URL(event.url).pathname
-      const p = new Path('/api/surveys/:surveyId/:choice')
-      console.log(p.test(pathname))
-    })
+    const p = new Path('/api/surveys/:surveyId/:choice')
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname)
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice
+          }
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value()
+
+    console.log(events)
     res.send({})
   })
 
